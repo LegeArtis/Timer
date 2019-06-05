3a9cf427ec5d37e5bd4d838f5ed446f6c92e4e22
Artem Kovalov <jasonswonabk@gmail.com>
2019-06-06 00:59:22 +0300

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {take, takeUntil} from 'rxjs/operators';
-import {interval, Observable} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import {interval} from 'rxjs';
 import {fromEvent} from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -13,44 +14,63 @@ export class AppComponent implements OnInit {
   timer = '00: 00: 00';
   settingTime;
 
+
   ngOnInit() {
-    const startClick = fromEvent(document.getElementById('start'), 'click');
-    startClick.subscribe(() => {
-      const waitClick = fromEvent(document.getElementById('wait'), 'click');
-      const resetClick = fromEvent(document.getElementById('reset'), 'click');
-      const stopClick = fromEvent(document.getElementById('start'), 'click');
+   const startClick = fromEvent(document.getElementById('start'), 'click'),
+         waitClick = fromEvent(document.getElementById('wait'), 'click'),
+         resetClick = fromEvent(document.getElementById('reset'), 'click');
+    let start = true,
+        wait = true;
 
-      stopClick.subscribe(() => {
-        this.time = this.settingTime;
-        this.secondsToTimerTime();
-      });
+      startClick.subscribe(() => {
 
-      resetClick.subscribe(() => {
-        this.time = 0;
-        this.timer = this.secondsToTimerTime();
-      });
+        if (start) {
+          start = false;
 
-      interval(1000)
-        .pipe(takeUntil(waitClick))
-        .pipe(takeUntil(resetClick))
-        .pipe(takeUntil(stopClick))
-        .subscribe(() => {
-          --this.time;
+          resetClick.subscribe(() => {
+            this.time = 0;
+            this.timer = this.secondsToTimerTime();
+          });
+
+          waitClick.subscribe(()  => {
+            if (wait) {
+              console.log('wait');
+              wait = false;
+            } else {
+              wait = true;
+              interval(1000)
+                .pipe(takeUntil(waitClick))
+                .pipe(takeUntil(resetClick))
+                .pipe(takeUntil(startClick))
+                .subscribe(() => {
+                  --this.time;
+                  this.timer = this.secondsToTimerTime();
+                });
+            }
+          });
+
+          interval(1000)
+            .pipe(takeUntil(waitClick))
+            .pipe(takeUntil(resetClick))
+            .pipe(takeUntil(startClick))
+            .subscribe(  () => {
+              --this.time;
+              this.timer = this.secondsToTimerTime();
+            });
+
+        } else {
+          start = true;
+          this.time = this.settingTime;
           this.timer = this.secondsToTimerTime();
+        }
       });
-    });
   }
 
   public secondsToTimerTime() {
-   const seconds = this.time % 60;
-   const minutes = Math.floor(this.time / ( 60 )) % 60;
-   const hours = (Math.floor( this.time / ( 60 * 60 )) % 24);
-
-   return `${hours < 10 ? '0' + hours : hours}: ${minutes < 10 ? '0' + minutes : minutes}: ${seconds < 10 ? '0' + seconds : seconds}`;
+   return new Date(this.time * 1000).toISOString().substr(11, 8);
   }
 
   public setTime(hours, minutes, seconds) {
-    console.log(seconds + ' ' + minutes + ' ' + hours );
     this.time = +seconds + +minutes * 60 + +hours * 60 * 60;
     if (this.time <= 0) {
       alert(`Кто-то ввел непрвильную циферку ! Время не может быть отрицателным или нулем!`);
